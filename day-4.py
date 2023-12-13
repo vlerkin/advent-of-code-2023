48405ad659ffd05fcda733f7e65b65ca5af08262
Valeriia Klestova <v.s.apukhtina@gmail.com>
2023-12-13 15:53:22 +0100

    added solution for day 4 part 2

--- day-4.py
+++ day-4.py
@@ -1,5 +1,6 @@
 
-with open('input.txt', 'r') as lines:
+# part 1
+"""with open('input.txt', 'r') as lines:
     total_points = 0
     for line in lines:
         card_points = 0
@@ -13,5 +14,29 @@ with open('input.txt', 'r') as lines:
             else:
                 card_points *= 2
         total_points += card_points
-    print(total_points)
+    print(total_points)"""
+
+# part 2
+with open('input.txt', 'r') as lines:
+    num_card_instances = {}
+    card_number = 0
+    for line in lines:
+        card_number += 1
+        if card_number not in num_card_instances:
+            num_card_instances[card_number] = 1
+        else:
+            num_card_instances[card_number] += 1
+        winning_nums, card_nums = line.strip().split(": ")[1].split(" | ")
+        winning_nums = winning_nums.split()
+        card_nums = card_nums.split()
+        nums = set(card_nums).intersection(set(winning_nums))
+        print(num_card_instances[card_number])
+        for j in range(num_card_instances[card_number]):
+            for i in range(len(nums)):
+                if (card_number + i + 1) not in num_card_instances:
+                    num_card_instances[card_number + i + 1] = 1
+                else:
+                    num_card_instances[card_number + i + 1] += 1
+    print(num_card_instances)
+    print(sum(num_card_instances.values()))
 
